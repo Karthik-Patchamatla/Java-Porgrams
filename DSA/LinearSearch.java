2758de95b831e3ac818289d9f919e07eefe3d9fe
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-11-15 09:20:22 +0530

    Linear Search

---
+++ DSA/LinearSearch.java
@@ -0,0 +1,19 @@
+package DSA;
+
+public class LinearSearch {
+    public static int Searching(int a[], int key) {
+        for(int i=0; i<a.length; i++) {
+            if(a[i]==key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int array[] = {10,20,30,40};
+        int key = 40;
+        int value = Searching(array, key);
+        System.out.println(String.format("The element %s is found at index %s", key,value));
+    }
+}
