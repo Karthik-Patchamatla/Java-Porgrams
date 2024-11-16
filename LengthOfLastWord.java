291007ab322c8a046c72afc6b60bf0403b59c301
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-11-16 10:41:12 +0530

    Length of the last word

---
+++ LengthOfLastWord.java
@@ -0,0 +1,13 @@
+class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int lastSpaceIndex = s.lastIndexOf(' '); 
+        return s.length() - lastSpaceIndex - 1;  
+    }
+
+    public static void main(String[] args) {
+        LengthOfLastWord obj = new LengthOfLastWord();
+        String s = "Karthik varma";
+        System.out.println(obj.lengthOfLastWord(s));
+    }
+}
