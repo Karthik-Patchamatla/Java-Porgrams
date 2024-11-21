5ee8bae75e185b24a1e06b416d63d2a727963b31
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-11-21 21:24:48 +0530

    Number Triangle Pattern

---
+++ Patterns/NumberTriangle.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class NumberTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(i+1 + " ");                
+            }
+            System.out.println();
+        }
+    }
+}
