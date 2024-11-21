3882066ab643b14b2cd32ab5e9e46428fe229f1c
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-11-21 22:04:37 +0530

    Rhombus Pattern

---
+++ Patterns/RhombusPattern.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class RhombusPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
